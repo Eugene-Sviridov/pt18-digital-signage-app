5adaaafb856a05196ed34afc5c15b3990ba721b4
Eugene-Sviridov <smart8410@gmail.com>
2018-06-06 02:46:45 +0300

    Size adjustment

--- src/components/VendorsList/index.js
+++ src/components/VendorsList/index.js
@@ -3,40 +3,126 @@ import st from './style.scss';
 
 export default class VendorsList extends Component {
     vendors = [
-        'assets/sponsors/huawei.png',
-        'assets/sponsors/microsoft.png',
-        'assets/sponsors/citrix.png',
-        'assets/sponsors/ikonic.png',
-        'assets/sponsors/mimecast.png',
-        'assets/sponsors/DigitalXRAID.png',
-        'assets/sponsors/bitdefender.png',
-        'assets/sponsors/s2s.png',
-        'assets/sponsors/dellemc.png',
-        'assets/sponsors/barracuda.png',
-        'assets/sponsors/powervision.png',
-        'assets/sponsors/virtual-college.png',
-        'assets/sponsors/lenovo.png',
-        'assets/sponsors/YHRCCU.png',
-        'assets/sponsors/canon.png',
-        'assets/sponsors/hp.png',
-        'assets/sponsors/DJI.png',
-        'assets/sponsors/black-marble.png',
-        'assets/sponsors/vapour-partner.png',
-        'assets/sponsors/ubtech.png',
-        'assets/sponsors/jct600.png',
-        'assets/sponsors/f-secure.png',
-        'assets/sponsors/zen.png',
-        'assets/sponsors/tesla.png',
-        'assets/sponsors/toshiba.png',
-        'assets/sponsors/symantec.png',
+        {
+            modificator: st.barracuda,
+            src: 'assets/sponsors/barracuda.png',
+        },
+        {
+            modificator: st.citrix,
+            src: 'assets/sponsors/citrix.png',
+        },
+        {
+            modificator: st.dell,
+            src: 'assets/sponsors/Dell EMC Partner Logos-02.png',
+        },
+        {
+            modificator: st.digitalxraid,
+            src: 'assets/sponsors/DigitalXRAID.png',
+        },
+        {
+            modificator: st.hp,
+            src: 'assets/sponsors/HP Partner Logos-03.png',
+        },
+        {
+            modificator: st.huawei,
+            src: 'assets/sponsors/Huawei Partner Logos-04.png',
+        },
+        {
+            modificator: st.ikonic,
+            src: 'assets/sponsors/ikonic logo.png',
+        },
+        {
+            modificator: st.jct600,
+            src: 'assets/sponsors/jct600 - logo.png',
+        },
+        {
+            modificator: st.lenovo,
+            src: 'assets/sponsors/lenovo.png',
+        },
+        {
+            modificator: st.bitdefender,
+            src: 'assets/sponsors/LOGO - Bitdefender whiteout 001.png',
+        },
+        {
+            modificator: st.canon,
+            src: 'assets/sponsors/LOGO - Canon white.png',
+        },
+        {
+            modificator: st.dji,
+            src: 'assets/sponsors/LOGO - DJI whiteout 001.png',
+        },
+        {
+            modificator: st.fsecure,
+            src: 'assets/sponsors/LOGO - F-secure whiteout 001.png',
+        },
+        {
+            modificator: st.microsoft,
+            src: 'assets/sponsors/LOGO - microsoft whiteout 001.png',
+        },
+        {
+            modificator: st.powervision,
+            src: 'assets/sponsors/LOGO - powervision - whiteout 001.png',
+        },
+        {
+            modificator: st.tesla,
+            src: 'assets/sponsors/LOGO - Tesla whiteout 001.png',
+        },
+        {
+            modificator: st.ubtech,
+            src: 'assets/sponsors/LOGO - ubtech - whiteout 001.png',
+        },
+        {
+            modificator: st.toshiba,
+            src: 'assets/sponsors/LOGO -Toshiba white out 001.png',
+        },
+        {
+            modificator: st.yhrccu,
+            src: 'assets/sponsors/LOGO YHRCCU (police) whiteout 001.png',
+        },
+        // {
+        //     modificator: st.mclaren,
+        //     src: 'assets/sponsors/mclaren - logo whiteout.png',
+        // },
+        {
+            modificator: st.mimecast,
+            src: 'assets/sponsors/mimecast.png',
+        },
+        {
+            modificator: st.marble,
+            src: 'assets/sponsors/pt18_black_marble.png',
+        },
+        {
+            modificator: st.symantec,
+            src: 'assets/sponsors/pt18_symantec.png',
+        },
+        {
+            modificator: st.s2s,
+            src: 'assets/sponsors/S2S logo.png',
+        },
+        // {
+        //     modificator: st.sphero,
+        //     src: 'assets/sponsors/Sphero - whiteout 001.png',
+        // },
+        {
+            modificator: st.vapour,
+            src: 'assets/sponsors/Vapour Partner Logos-11.png',
+        },
+        {
+            modificator: st.virtualCollege,
+            src: 'assets/sponsors/virtual college logo.png',
+        },
+        {
+            modificator: st.zen,
+            src: 'assets/sponsors/Zen Office Main Logo B+W - reversed.png',
+        },
     ]
     render() {
         return (
             <ul className={st.vendorsList}>
                 {
-                    this.vendors.map((src, i) => (
+                    this.vendors.map((vendor, i) => (
                         <li className={st.vendorEl} key={i}>
-                            <img src={src} />
+                            <img className={`${st.vendorImg} ${vendor.modificator}`} src={vendor.src} />
                         </li>
                     ))
                 }
--- src/routes/Speakers/SpeakersArray.js
+++ src/routes/Speakers/SpeakersArray.js
@@ -11,32 +11,35 @@ const speakers = [
     },
     {
         id: 1,
-        name: 'Will Greenwood.',
-        title: 'Guest speaker.',
-        info: 'With a professional career that spanned 11 years and 55 international caps will established a reputation as an unsurpassed mid-field creator. He was an integral part of England’s victorious World Cup campaign of 2003, a character and leader in one of the most successful team dynamics seen in recent times. His business credentials are equally impressive.',
-        src: '/assets/speakers/will-greenwood.png'
-    },
-    {
-        id: 2,
-        name: 'David Tweedale & Liam Hopwood.',
-        titleImg: '/assets/logos/mimecast.png',
-        info: 'David and Liam have been members of the Mimecast Channel Team for over 3 years, focused on developing the relationship between Mimecast and MSP\'s and developing new security solutions. The duo will take the stage to discuss the evolving threat landscape, what can be done to secure the future of IT and talk about how mimecast can assist in the design and architecture of solutions that help keep customer\'s safe.',
-        src: '/assets/speakers/david-and-liam.png'
-    },
-    {
-        id: 3,
         name: 'Sarah Shields.',
-        smallTitle: 'DellEMC',
+        smallTitle: 'On stage: 11:15 - 12:00',
         info: "Sarah Shields is one of the leading female businesswomen in the UK. Sarah has been at Dell EMC since 2008 and has traversed through the company's consumer, retail, small business and now channel organisations. A strong track record of success, growth and strong leadership, Sarah is known for her sharp thinking, sense of humour and drive for success.",
         titleImg: '/assets/logos/dellemc.png',
         src: '/assets/speakers/sarah.png'
     },
     {
-        id: 4,
+        id: 2,
         name: 'Neil Sawyer.',
+        smallTitle: 'On stage: 13:00 - 13:30',
         titleImg: '/assets/logos/hp.png',
         info: 'Neil Sawyer leads the UK&I Channel for HP in the UK. sales Responsible for all aspects of HP\'s Printing, Personal Systems, Mobility and Software Solutions portfolio as well as Distribution, Corporate SI’s, ISV’s and the SME/Education reseller channel, Neil also carries the revenue responsibility for the Education and Mid-Market End User Sales team.',
         src: '/assets/speakers/neil.png'
+    },
+    {
+        id: 3,
+        name: 'David Tweedale & Liam Hopwood.',
+        titleImg: '/assets/logos/mimecast.png',
+        smallTitle: 'On stage: 14:15 - 14:45',
+        info: 'David and Liam have been members of the Mimecast Channel Team for over 3 years, focused on developing the relationship between Mimecast and MSP\'s and developing new security solutions. The duo will take the stage to discuss the evolving threat landscape, what can be done to secure the future of IT and talk about how mimecast can assist in the design and architecture of solutions that help keep customer\'s safe.',
+        src: '/assets/speakers/david-and-liam.png'
+    },
+    {
+        id: 4,
+        name: 'Will Greenwood.',
+        title: 'Guest speaker.',
+        smallTitle: 'On stage: 14:45 - 15:15',
+        info: 'With a professional career that spanned 11 years and 55 international caps will established a reputation as an unsurpassed mid-field creator. He was an integral part of England’s victorious World Cup campaign of 2003, a character and leader in one of the most successful team dynamics seen in recent times. His business credentials are equally impressive.',
+        src: '/assets/speakers/will-greenwood.png'
     }
 ]
 
